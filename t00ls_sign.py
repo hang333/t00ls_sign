cde9a9eb986ba895f7de4dc2737b4ad1e013fc89
hang333 <jonathan.goh333@gmail.com>
2020-03-20 18:10:20 +0800

    增加安全性

--- t00ls_sign.py
+++ t00ls_sign.py
@@ -63,7 +63,11 @@ def login_t00ls(session):
         'redirect': 'https://www.t00ls.net',
         'cookietime': '2592000'
     }
+
+    censored_data = data
+    censored_data['password'] = '***'
     print(data)
+    
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
     headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     headers['Referer'] = 'https://www.t00ls.net/login.html'
